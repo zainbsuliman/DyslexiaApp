09b2a330978cb1dc0e7a39deed105cf404f4feb5
zainbsuliman <zat301099@gmail.com>
2024-12-12 09:35:36 +0300

    Initial Commit

---
+++ DyslexiaApp/DyslexiaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DyslexiaAppApp.swift
+//  DyslexiaApp
+//
+//  Created by Zainab Alatwi  on 11/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct DyslexiaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
