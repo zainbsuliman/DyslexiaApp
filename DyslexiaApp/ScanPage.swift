6ccb7f70914215e87a5bb879e5bca82ff4368ab4
zainbsuliman <zat301099@gmail.com>
2024-12-12 09:58:50 +0300

    all the views

---
+++ DyslexiaApp/ScanPage.swift
@@ -0,0 +1,18 @@
+//
+//  ScanPage.swift
+//  DyslexiaApp
+//
+//  Created by Zainab Alatwi  on 11/06/1446 AH.
+//
+
+import SwiftUI
+
+struct ScanPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanPage()
+}
