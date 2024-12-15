27cc9b932fe9c61af24261f19bcb0b45103cd030
zainbsuliman <zat301099@gmail.com>
2024-12-15 11:45:48 +0300

    all

---
+++ DyslexiaApp/ViewModel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DyslexiaApp
+//
+//  Created by Zainab Alatwi  on 11/06/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
--- DyslexiaApp/ViewModel/ScanPage.swift
+++ DyslexiaApp/ViewModel/ScanPage.swift
@@ -1,5 +1,5 @@
 //
-//  Onbording3.swift
+//  ScanPage.swift
 //  DyslexiaApp
 //
 //  Created by Zainab Alatwi  on 11/06/1446 AH.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct Onbording3: View {
+struct ScanPage: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    Onbording3()
+    ScanPage()
 }
